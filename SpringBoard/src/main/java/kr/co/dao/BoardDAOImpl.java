97d975ec745141d96d0781d63fef54a5e6d81bf8
kis3744 <kis3744@naver.com>
2020-02-08 02:48:58 +0900

    개발환경 setting  + 게시물작성,목록보기 구현

---
+++ SpringBoard/src/main/java/kr/co/dao/BoardDAOImpl.java
@@ -0,0 +1,30 @@
+package kr.co.dao;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+
+import kr.co.vo.BoardVO;
+
+@Repository
+public class BoardDAOImpl implements BoardDAO {
+
+	@Inject
+	private SqlSession sqlSession;
+	
+	@Override
+	public void write(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		sqlSession.insert("boardMapper.insert" , boardVO);
+		
+	}
+	
+	@Override
+	public List<BoardVO> list() throws Exception{
+		return sqlSession.selectList("boardMapper.list");
+	}
+
+}
---
+++ SpringBoard/src/main/java/kr/co/service/BoardServiceImpl.java
@@ -0,0 +1,28 @@
+package kr.co.service;
+
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.springframework.stereotype.Service;
+
+import kr.co.dao.BoardDAO;
+import kr.co.vo.BoardVO;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+	@Inject
+	private BoardDAO dao;
+	
+	@Override
+	public void write(BoardVO boardVO) throws Exception {
+		// TODO Auto-generated method stub
+		dao.write(boardVO);
+	}
+	
+	@Override
+	public List<BoardVO> list() throws Exception{
+		return dao.list();
+	}
+}
